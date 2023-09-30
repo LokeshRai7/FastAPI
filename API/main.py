171cfcfeffff1d088eb149d3f744516b54072fcb
Lokesh Rai <76644260+LokeshRai7@users.noreply.github.com>
2023-09-30 21:21:42 +0530

    updated post class with more validation fields

--- API/main.py
+++ API/main.py
@@ -1,12 +1,15 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+
+
 app = FastAPI()
 class post(BaseModel):
     title: str
     content: str
-
-
+    published: bool = True
+    rating: Optional[int] = None
 
 
 @app.get("/")
@@ -19,5 +22,6 @@ async def get_post():
 
 @app.post("/createPost")
 async def create_posts(new_post: post):
-    print(new_post)
+    print(new_post.rating)
+    print(new_post.dict())
     return{"data":"new post lovely"}
