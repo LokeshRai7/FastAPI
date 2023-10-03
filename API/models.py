a6276db68085a326d02c8b672dfbee923c9cdedc
Lokesh Rai <76644260+LokeshRai7@users.noreply.github.com>
2023-10-03 20:42:56 +0530

    using orm to perform queries - get all posts

--- API/models.py
+++ API/models.py
@@ -1,10 +1,10 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from .database import Base
 from sqlalchemy.sql.expression import null
+from sqlalchemy.orm import relationship
 
 class Post(Base):
     __tablename__ = "posts"
-    
     id  = Column(Integer, primary_key = True, nullable = False)
     title = Column(String ,nullable = False, default = "Title")
-    published = Column(Boolean, nullable = False, default = True)
+    published = Column(Boolean, default = True)
