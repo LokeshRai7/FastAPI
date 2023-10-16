4e32edfd3c3dbb10326611f717060370b8f01c66
Lokesh Rai <76644260+LokeshRai7@users.noreply.github.com>
2023-10-16 23:51:40 +0530

    removed redundant code

--- API/main.py
+++ API/main.py
@@ -1,24 +1,12 @@
 from typing import Optional
 from fastapi import FastAPI, Response, status, HTTPException, Depends
-# from fastapi.params import Body
-from pydantic import BaseModel
-# from random import randrange
-# import psycopg2
-# from psycopg2.extras import RealDictCursor
-# import time
-
 from sqlalchemy.orm import Session
-from . import models
+from . import models, schemas
 from .database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
 
 @app.get("/")
 async def root():
@@ -31,7 +19,7 @@ async def get_post(db: Session = Depends(get_db)):
     return {"data": posts}
 
 @app.post("/posts",status_code=status.HTTP_201_CREATED)
-async def create_posts(post:Post,db: Session = Depends(get_db)):
+async def create_posts(post:schemas.PostCreate,db: Session = Depends(get_db)):
     
     newPost = models.Post(**post.dict())
     db.add(newPost)
@@ -81,9 +69,3 @@ def updatePost(id: int, updated_posts: Post, db: Session = Depends(get_db)):
     db.commit()
 
     return {"data":post_query.first()}
-
-
-# @app.get("/sqlalchemy")
-# def testPosts(db: Session = Depends(get_db)):
-#     posts = db.query(models.Post).all()
-#     return {"data":posts}
