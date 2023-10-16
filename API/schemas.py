dd384223be7b2d243a24ee35e5fc1e138e145a5e
Lokesh Rai <76644260+LokeshRai7@users.noreply.github.com>
2023-10-17 00:10:04 +0530

    debugged pydantic response model

--- API/schemas.py
+++ API/schemas.py
@@ -7,13 +7,13 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
+    created_at: datetime
     class Config:
         orm = True
 
 class PostBase(BaseModel):
     title: str
     content: str
-    created_at: datetime
     published: bool = True
 
 class PostCreate(PostBase):
