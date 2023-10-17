ba52ee22c3c28bef6a5d48ae335c00b8ef3eefba
Lokesh Rai <76644260+LokeshRai7@users.noreply.github.com>
2023-10-17 19:40:23 +0530

    Added Requirements.txt and enforced unique constraint on email table

--- API/models.py
+++ API/models.py
@@ -1,5 +1,5 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, TIMESTAMP, Text, text
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, TIMESTAMP, Text, text, UniqueConstraint
 from sqlalchemy.sql.expression import null
 from sqlalchemy.orm import relationship
 
@@ -10,3 +10,13 @@ class Post(Base):
     content = Column(String,nullable = False)
     published = Column(Boolean, server_default = 'TRUE',nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer,primary_key = True,nullable = False)
+    email = Column(String ,unique = True,nullable = False)
+    password = Column(String, nullable = False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    __table_args__ = (UniqueConstraint('email'),)
