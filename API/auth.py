fd2bb3ac5f13fd07bf78167e0fa70c657046385e
Lokesh Rai <76644260+LokeshRai7@users.noreply.github.com>
2023-10-17 22:13:54 +0530

    added oauth2 feature with jwt token for verification on login

--- API/auth.py
+++ API/auth.py
@@ -1,14 +1,14 @@
 from fastapi import Response, status, HTTPException, Depends, APIRouter
-from . import models, schemas, utils
+from . import models, schemas, utils, oauth2
 from sqlalchemy.orm import Session
 from .database import engine, get_db
-
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 router = APIRouter(tags=["Authentication"])
 
 @router.get('/login')
-def login(user_credentials: schemas.userLogin,db: Session = Depends(get_db)):
-       user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(),db: Session = Depends(get_db)):
+       user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
        if not user:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
@@ -17,8 +17,9 @@ def login(user_credentials: schemas.userLogin,db: Session = Depends(get_db)):
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
        
        # create a token
+       access_token = oauth2.create_Access_Token(data={"user.id": user.id})
 
        # return a token
 
-       return {"token": "example token"}
+       return {"access_token":access_token, "token_type":"bearer" }
 
